e1d2359576852df1d10c97681c9542c4a0b432ca
marielaveron <maravsf@gmail.com>
2024-07-01 21:40:49 -0300

    commit 00

---
+++ js/datos.js
@@ -0,0 +1,32 @@
+let personajes = [
+  {
+    nombre: "Adrián",
+    imagen: "",
+    bando: "Sweet Dreams",
+    roleadora: "Mara",
+    edad: "26",
+    signo: "tauro",
+    biografia:
+      "Llego a Asylum cuando se entero que su hermana, Sarah, fue ingresada al hospital por una sobredosis. Trabaja para los Sweet Dreams, le encanta disfrutar del momento y es un putero nato.",
+    familia: "Hermana: Sarah",
+    amigos: "Isidoro, Bo, Luis",
+    trabajo: "Traficante para los Sweet Dreams",
+    historia:
+      "El y su hermana gemela nacieron en Argentina pero a una edad muy joven su padre los llevó a Los Santos para tener una mejor vida, pero pronto ambos fueron abandonados a su suerte por este. Los gemelos lograron sobrevivir trabajando desde jóvenes para maleantes, hasta que ambos se separaron por una pelea. Adrián se fue a vivir a Marbella, donde conoció a Bo en una carrera inlegal, donde este le ayudo a escapar de la policia. Desde entonces trabaja para él y se hizo gran amigo de este y varios integrantes de la banda.",
+  },
+  {
+    nombre: "Sarah",
+    imagen: "",
+    bando: "Desconocido",
+    roleadora: "Mara",
+    edad: "26",
+    signo: "tauro",
+    biografia:
+      "Después de que su esposa le pidiera el divorció, tiene una sobredosis y queda en coma por un tiempo. Al final despierta, pero tiene perdida de memoria, olvidandose de su mujer y todo lo vivido en el pueblo.",
+    familia: "Hermano: Adrian",
+    amigos: "Desconocido",
+    trabajo: "Desconocido",
+    historia:
+      "El y su hermano gemelo nacieron en Argentina pero a una edad muy joven su padre los llevó a Los Santos para tener una mejor vida, pero pronto ambos fueron abandonados a su suerte por este. Los gemelos lograron sobrevivir trabajando desde jóvenes para maleantes, hasta que ambos se separaron por una pelea. Sarah fue militar pero fue echada del cuerpo luego de agredir a un superior, quemandole el rostro. Se caso al poco tiempo de llegar a Asylum, pero luego de ser atrapada coqueteando con otras mujeres, su esposa le pide el divorcio y aborta el hijo que estaban esperando.",
+  },
+];
